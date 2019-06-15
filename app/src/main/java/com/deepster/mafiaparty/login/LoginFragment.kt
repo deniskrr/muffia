23b1a53ad2d30c36eb77f25b1af7071f747172bc
deepster <craciunescu.denis12@gmail.com>
2019-06-15 16:31:14 +0300

    Add login functionality

--- app/src/main/java/com/deepster/mafiaparty/login/LoginFragment.kt
+++ app/src/main/java/com/deepster/mafiaparty/login/LoginFragment.kt
@@ -2,14 +2,25 @@ package com.deepster.mafiaparty.login
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 
 import com.deepster.mafiaparty.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : androidx.fragment.app.Fragment() {
+
+    companion object {
+        val TAG = LoginFragment::class.java.simpleName
+    }
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +28,36 @@ class LoginFragment : androidx.fragment.app.Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser != null) { // User is logged in
+            val action = LoginFragmentDirections.actionLoginFragmentToMainFragment()
+            findNavController().navigate(action)
+        }
+
+        button_login.setOnClickListener { button ->
+            button.isEnabled = false
+            val email = email_login.editText!!.text.toString()
+            val password = password_login.editText!!.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "Sign in success")
+                    val action = LoginFragmentDirections.actionLoginFragmentToMainFragment()
+                    button.findNavController().navigate(action)
+
+                } else {
+                    Log.d(TAG, "Sign in failure")
+                    button.isEnabled = true
+                }
+            }
+        }
+
     }
 
 
