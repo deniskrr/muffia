584224cbbc3753f1bd2e5baa1d58cb00a232afb4
deepster <craciunescu.denis12@gmail.com>
2019-06-17 14:09:13 +0300

    Add StartGame cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,25 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
 
+admin.initializeApp()
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+// Start game when
+export const startGame = functions.firestore.document("games/{roomID}")
+.onUpdate(change => {
+        let after = change.after.data()
+
+        // If there are 7 players in the lobby, start the game
+        if (after !== undefined && Object.keys(after.players).length == 7) {            
+            after.period = "DAY_ONE"
+            change.before.ref.update(after).then().catch()
+        }
+        return null;
+    })
+
