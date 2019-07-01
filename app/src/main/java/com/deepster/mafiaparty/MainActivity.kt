f9a76e03c46d41338b0a2c6c3627f9398d14ed6e
deepster <craciunescu.denis12@gmail.com>
2019-07-01 21:37:04 +0300

    Add menu

--- app/src/main/java/com/deepster/mafiaparty/MainActivity.kt
+++ app/src/main/java/com/deepster/mafiaparty/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.deepster.mafiaparty
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+
 }
